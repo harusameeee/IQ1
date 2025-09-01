ff4ec138af7d11e80b4ebe03275b03b96c52034a
haru0sameee <it232183@trident.ac.jp>
2025-09-01 12:50:34 +0900

    要らないファイル消し、危険時演出

---
+++ Assets/Tsuji/UI/Script/SceneChanger.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ToSelect()
+    {
+        SceneManager.LoadScene("SelectScene");
+    }
+
+    public void ToPlay()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void ToResult()
+    {
+        SceneManager.LoadScene("ResultScene");
+    }
+
+    public void ToEnd()
+    {
+        Application.Quit();
+    }
+
+    public void OpenOption()
+    {
+        //オプションを呼び出す
+    }
+}
