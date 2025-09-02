9027ff2cd0a2d7036249372176b4ffde6cca654c
kyota1212 <150208911+kyota1212@users.noreply.github.com>
2025-09-02 11:52:07 +0900

    escで終わるやつ、フェード、フルスク表示・解像度対応

---
+++ Assets/yamada/script/GameManager.cs
@@ -0,0 +1,28 @@
+//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 60FPSに制限を与える
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Escが押された時
+        if (Input.GetKey(KeyCode.Escape))
+        {
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+#else
+    Application.Quit();//ゲームプレイ終了
+#endif
+        }
+    }
+}
