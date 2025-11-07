b7385cc2dc97f96207e60d33416d70c1c8630f56
Hshusu <96405620+Hshusu@users.noreply.github.com>
2025-11-07 13:25:30 +0800

    witch movement

--- Assets/Marcus/scripts/enemycode/enemypattern.cs
+++ Assets/Marcus/scripts/enemycode/enemypattern.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "enemypattern", menuName = "ScriptableObjects/enemypattern", order = 1)]
 public class enemypattern : ScriptableObject
 {
+    public Vector2 position = Vector2.zero;
+    public float duration = 6.0f;
     public List<enemypatterndata> patterndata = new List<enemypatterndata>();
 }
 [System.Serializable]
