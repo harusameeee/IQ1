6aea21ddb859662f97457c74ea91f755f0aea965
tarou102 <kshin102@icloud.com>
2025-12-19 11:40:19 +0900

    スプライン修正

--- Assets/Marcus/scripts/obstacle.cs
+++ Assets/Marcus/scripts/obstacle.cs
@@ -2,41 +2,60 @@ using UnityEngine;
 
 public class obstacle : hitbox
 {
-    public float tvalue = 0;
-    public Vector2 range_window = new Vector2(1.0f, 2.0f);//x represents the start range and y represents the end range
+    public float tvalue = 0f;
+
+    public Sprite sprite;
+
+    public Vector2 range_window = new Vector2(1.0f, 2.0f);
     public float despawn_range = 10.0f;
-    public Vector2 pos = new Vector2(0, 0);
-    public player_mover player;
+
+    public float lifeTime = 8.0f;   
+    float lifeTimer = 0f;
+
+    public Vector2 pos = Vector2.zero;
     public Vector2 dim = new Vector2(1, 1);
+
+    public player_mover player;
+    public hitboxvisualizer hitboxvis;
+
     public override Vector2 position => pos;
     public override Vector2 dimension => dim;
-    public Sprite sprite=null;
-    public hitboxvisualizer hitboxvis;//will remove later
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public virtual void  Start()
+
+    void Start()
     {
+        active = false;
     }
 
-    // Update is called once per frame
     public override void FixedUpdate()
     {
+        lifeTimer += Time.fixedDeltaTime;
 
-        if (tvalue > player.current_t - range_window.x && tvalue < player.current_t + range_window.y)
+        float splineLength = player.splinecont.Spline.GetLength();
+
+        float dt = tvalue - player.current_t_normalized;
+        if (dt < 0f) dt += 1f;
+
+        float dist = dt * splineLength;
+
+        // activation window
+        if (dist >= -range_window.x && dist <= range_window.y)
         {
-            base.FixedUpdate();
             active = true;
-        }
-        else if (tvalue <= player.current_t - despawn_range)
-        {
-            Debug.Log("Obstacle Despawned");
-            hitboxvis.additionalhitboxes.Remove(hitboxvis.additionalhitboxes.Find(hb => hb.todraw == this));
-            Destroy(this.gameObject);
+            base.FixedUpdate();
         }
         else
         {
             active = false;
         }
-        
 
+        // despawn by distance OR time
+        if (dist < -despawn_range || lifeTimer > lifeTime)
+        {
+            hitboxvis.additionalhitboxes.Remove(
+                hitboxvis.additionalhitboxes.Find(hb => hb.todraw == this)
+            );
+
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Marcus/scripts/obstacle_spawner.cs
+++ Assets/Marcus/scripts/obstacle_spawner.cs
@@ -1,71 +1,89 @@
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-
 using Random = System.Random;
+
 public class obstacle_spawner : MonoBehaviour
 {
-    public float spawnfrequency = 2.0f;
-    public float item_spawn_frequency = 5.0f;
-    public float item_timer = 0.0f;
+    // -------- spawn timing --------
+    public float spawnfrequency = 2.0f;          // obstacle spawn interval
+    public float item_spawn_frequency = 5.0f;    // item spawn interval
     public float timer = 0.0f;
-    public int maxoffset = 5;
-    public float mindist = 5;
-    public float offset_dist = 50;
+    public float item_timer = 0.0f;
+
+    // -------- lane / offset --------
+    public int maxoffset = 5;                     // max lane offset
+    public float mindist = 5;                     // minimum distance between lanes
+    public float offset_dist = 50;                // distance forward on spline
+
+    // -------- references --------
     public Transform indicator_transform;
-    public hitboxvisualizer hitboxvis;//will remove later
+    public hitboxvisualizer hitboxvis;
+    public player_mover player;
+
+    // -------- pools --------
     public List<GameObject> obstacles = new List<GameObject>();
     public List<GameObject> items = new List<GameObject>();
     public List<indicator> indicators = new List<indicator>();
 
-    static public List<entity> damagables = new List<entity>();
-    public List<Sprite> obstacle_sprites = new List<Sprite>();
-    public player_mover player;
+    // -------- entities --------
+    public static List<entity> damagables = new List<entity>();
+
     float indicator_countdown = 7.0f;
-    //note need to allow for making double wide obstacles
-    //need to make indicator for where obstacle will spawn
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // ----------------------------------------
     void Awake()
     {
         damagables = new List<entity>();
         entity.onspawn += addtodamagables;
     }
+
     void Start()
     {
+        // indicator pool
         for (int i = 0; i < 30; i++)
         {
-            var indicatorobj = Instantiate(Resources.Load<GameObject>("indicator"), indicator_transform);
-            indicatorobj.SetActive(false);
-            indicators.Add(indicatorobj.GetComponent<indicator>());
+            var obj = Instantiate(
+                Resources.Load<GameObject>("indicator"),
+                indicator_transform
+            );
+
+            obj.SetActive(false);
+            indicators.Add(obj.GetComponent<indicator>());
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
         item_timer += Time.deltaTime;
+
         if (timer > spawnfrequency)
         {
             spawnobstacle();
         }
+
         if (item_timer > item_spawn_frequency)
         {
             spawnitem();
         }
     }
+
+    // ----------------------------------------
     void addtodamagables(entity d)
     {
-        Debug.Log($"Hitbox registering {d.name} to damagables");
         if (!damagables.Contains(d))
         {
             damagables.Add(d);
         }
     }
+
+    // ----------------------------------------
     void spawnobstacle()
     {
         Random rng = new Random(System.DateTime.Now.Millisecond);
-        timer = (float)rng.Next(-3, 3) / 10;
+        timer = (float)rng.Next(-3, 3) / 10f;
+
+        // find unused indicator
         indicator obstacleindicator = null;
         foreach (var ind in indicators)
         {
@@ -75,115 +93,155 @@ public class obstacle_spawner : MonoBehaviour
                 break;
             }
         }
-            bool validpos = false;
-            int attempts = 0;
-            
 
-            float temp = 0;
-            while(!validpos && attempts<10)
-            {
-                rng = new Random(System.DateTime.Now.Millisecond * 2 + 1 + attempts);
-                temp = (float)rng.Next(-maxoffset, maxoffset);
-                foreach(indicator ind in indicators)
-                {
-                    if (ind.gameObject.activeSelf)
-                    {
-                        if (Mathf.Abs(ind.offsetpos- temp) > mindist)
-                        {
-                            validpos = true;
-                            break;
-                        }
-                        else
-                        {
-                            validpos = false;
-                        }
-                    }
-                }
-                attempts++;
-            }
+        // decide lane offset
+        bool validpos = false;
+        int attempts = 0;
+        float temp = 0;
 
-        Vector4 spawnpos = player.getobstaclespawnpos(temp, offset_dist, out bool valid, out float new_t);
-        if (!valid)
+        while (!validpos && attempts < 10)
         {
+            rng = new Random(System.DateTime.Now.Millisecond + attempts);
+            temp = rng.Next(-maxoffset, maxoffset);
 
-            return;
-        }
-        obstacle obs = Instantiate(obstacles[rng.Next(0, obstacles.Count)]).GetComponent<obstacle>();
-        obs.player = player;
-        obs.pos.x = temp;
-        hitboxvis.additionalhitboxes.Add(new hitboxvisualizer.hitboxpair { todraw = obs, hbcolor = Color.red });
-        obs.tvalue = new_t;
-        obs.hitboxvis = hitboxvis;
-        obs.reftransform = player.transform;
-        obstacleindicator.transform.localPosition = new Vector3(temp * 80, 0, 0);
-        obstacleindicator.gameObject.SetActive(true);
-        obs.transform.position = new Vector3(spawnpos.x, spawnpos.y, spawnpos.z);
-        obs.transform.rotation = Quaternion.Euler(0, spawnpos.w, 0);
-        obstacleindicator.setvalues(player, obs.transform, temp);
-    }
-    void spawnitem()
-    {
-            Random rng = new Random(System.DateTime.Now.Millisecond*2);
-            item_timer = (float)rng.Next(-3, 3) / 10;
-            indicator itemindicator = null;
-            foreach (var ind in indicators)
+            validpos = true;
+            foreach (indicator ind in indicators)
             {
-                if (!ind.gameObject.activeSelf)
+                if (ind.gameObject.activeSelf)
                 {
-                    itemindicator = ind;
-                    break;
-                }
-            }
-            bool validpos = false;
-            int attempts = 0;
-            
-
-            float temp = 0;
-            while(!validpos && attempts<10)
-            {
-                rng = new Random(System.DateTime.Now.Millisecond * 2 + 1 + attempts);
-                temp = (float)rng.Next(-maxoffset, maxoffset);
-                
-                foreach(indicator ind in indicators)
-                {
-                    if (ind.gameObject.activeSelf)
+                    if (Mathf.Abs(ind.offsetpos - temp) < mindist)
                     {
-                        if (Mathf.Abs(ind.offsetpos- temp) > mindist)
-                        {
-                            validpos = true;
-                        }
-                        else
-                        {
                         validpos = false;
                         break;
-                        }
                     }
                 }
-                attempts++;
             }
+            attempts++;
+        }
 
+        // get spawn position on spline (t-driven)
+        Vector4 spawnpos = player.getobstaclespawnpos(
+            temp,
+            offset_dist,
+            out bool valid,
+            out float new_t
+        );
 
+        if (!valid)
+            return;
 
-            Vector3 spawnpos = player.getobstaclespawnpos(temp, offset_dist, out bool valid, out float new_t);
-            if (!valid)
+        // create obstacle
+        obstacle obs = Instantiate(
+            obstacles[rng.Next(0, obstacles.Count)]
+        ).GetComponent<obstacle>();
+
+        obs.player = player;
+        obs.pos.x = temp;
+        obs.tvalue = new_t;
+        obs.reftransform = player.transform;
+        obs.hitboxvis = hitboxvis;
+
+        hitboxvis.additionalhitboxes.Add(
+            new hitboxvisualizer.hitboxpair
             {
-
-                return;
+                todraw = obs,
+                hbcolor = Color.red
             }
-            obstacle obs = Instantiate(items[rng.Next(0, items.Count)]).GetComponent<obstacle>();
-            obs.player = player;
-            obs.pos.x = temp;
-            hitboxvis.additionalhitboxes.Add(new hitboxvisualizer.hitboxpair { todraw = obs, hbcolor = Color.yellow });
-            obs.tvalue = new_t;
-            obs.hitboxvis = hitboxvis;
-            obs.reftransform = player.transform;
-            itemindicator.transform.localPosition = new Vector3(temp * 80, 0, 0);
-            itemindicator.gameObject.SetActive(true);
-            itemindicator.setitem(obs.sprite);
-            obs.transform.position = spawnpos;
-            obs.transform.rotation = player.transform.rotation;
-            itemindicator.setvalues(player, obs.transform, temp);
-    }
-    
+        );
 
+        // indicator
+        obstacleindicator.transform.localPosition =
+            new Vector3(temp * 80f, 0f, 0f);
+
+        obstacleindicator.gameObject.SetActive(true);
+        obstacleindicator.setvalues(player, obs.transform, temp);
+
+        // final transform
+        obs.transform.position =
+            new Vector3(spawnpos.x, spawnpos.y, spawnpos.z);
+
+        obs.transform.rotation =
+            Quaternion.Euler(0f, spawnpos.w, 0f);
+    }
+
+    // ----------------------------------------
+    void spawnitem()
+    {
+        Random rng = new Random(System.DateTime.Now.Millisecond * 2);
+        item_timer = (float)rng.Next(-3, 3) / 10f;
+
+        indicator itemindicator = null;
+        foreach (var ind in indicators)
+        {
+            if (!ind.gameObject.activeSelf)
+            {
+                itemindicator = ind;
+                break;
+            }
+        }
+
+        bool validpos = false;
+        int attempts = 0;
+        float temp = 0;
+
+        while (!validpos && attempts < 10)
+        {
+            rng = new Random(System.DateTime.Now.Millisecond + attempts);
+            temp = rng.Next(-maxoffset, maxoffset);
+
+            validpos = true;
+            foreach (indicator ind in indicators)
+            {
+                if (ind.gameObject.activeSelf)
+                {
+                    if (Mathf.Abs(ind.offsetpos - temp) < mindist)
+                    {
+                        validpos = false;
+                        break;
+                    }
+                }
+            }
+            attempts++;
+        }
+
+        Vector4 spawnpos = player.getobstaclespawnpos(
+            temp,
+            offset_dist,
+            out bool valid,
+            out float new_t
+        );
+
+        if (!valid)
+            return;
+
+        obstacle obs = Instantiate(
+            items[rng.Next(0, items.Count)]
+        ).GetComponent<obstacle>();
+
+        obs.player = player;
+        obs.pos.x = temp;
+        obs.tvalue = new_t;
+        obs.reftransform = player.transform;
+        obs.hitboxvis = hitboxvis;
+
+        hitboxvis.additionalhitboxes.Add(
+            new hitboxvisualizer.hitboxpair
+            {
+                todraw = obs,
+                hbcolor = Color.yellow
+            }
+        );
+
+        itemindicator.transform.localPosition =
+            new Vector3(temp * 80f, 0f, 0f);
+
+        itemindicator.gameObject.SetActive(true);
+        itemindicator.setitem(obs.sprite);
+        itemindicator.setvalues(player, obs.transform, temp);
+
+        obs.transform.position =
+            new Vector3(spawnpos.x, spawnpos.y, spawnpos.z);
+
+        obs.transform.rotation = player.transform.rotation;
+    }
 }
