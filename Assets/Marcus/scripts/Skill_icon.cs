7cda39f8d04bd9e3eef130af01a9312d55d26daf
haru0sameee <it232183@trident.ac.jp>
2025-12-23 11:43:49 +0900

    幻の4直し&マーライオンアニメーション&ボタン表示

--- Assets/Marcus/scripts/Skill_icon.cs
+++ Assets/Marcus/scripts/Skill_icon.cs
@@ -7,12 +7,12 @@ public class Skill_icon : MonoBehaviour
     public skilldata skill;
     public PlayerLineMove owner;
     public Image skill_icon;
-    public Transform stacks_transform;
+   // public Transform stacks_transform;
 
     public Transform coincost_transform;
 
     public Image gcd_icon;
-    public TMP_Text stacks_text;
+   // public TMP_Text stacks_text;
     public TMP_Text coincost_text;
     public TMP_Text cooldown_text;
 
@@ -30,10 +30,10 @@ public class Skill_icon : MonoBehaviour
         {
             float gcdper = 1-owner.gcd_timer / owner.current_max_gcd;
             gcd_icon.material.SetFloat("_removesegment", gcdper);
-            if (stacks_transform.gameObject.activeSelf)
-            {
-                stacks_text.text = skill.currentstacks.ToString();
-            }
+            //if (stacks_transform.gameObject.activeSelf)
+            //{
+            //    stacks_text.text = skill.currentstacks.ToString();
+            //}
             if (skill.has_cooldown && skill.currentcooldown > 0 && skill.maxstacks > skill.currentstacks)
             {
                 cooldown_text.text = Mathf.Ceil(skill.currentcooldown).ToString();
@@ -49,15 +49,15 @@ public class Skill_icon : MonoBehaviour
         owner = o;
         skill = s;
         skill_icon.sprite = skill.icon;
-        if (skill.maxstacks > 1)
-        {
-            stacks_transform.gameObject.SetActive(true);
-            stacks_text.text = skill.currentstacks.ToString();
-        }
-        else
-        {
-            stacks_transform.gameObject.SetActive(false);
-        }
+        //if (skill.maxstacks > 1)
+        //{
+        //    stacks_transform.gameObject.SetActive(true);
+        //    stacks_text.text = skill.currentstacks.ToString();
+        //}
+        //else
+        //{
+        //    stacks_transform.gameObject.SetActive(false);
+        //}
         if (skill.coincost > 0)
         {
             coincost_transform.gameObject.SetActive(true);
